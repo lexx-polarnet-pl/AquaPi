d5015599639db35e809962113a1f51a6936c4bd3
lexx-polarnet-pl <lexx@polarnet.pl>
2012-11-26 21:06:34 +0100

    kilka plików dodanych i instrukcja instalacji

---
+++ Software/daemon/aquapi.h
@@ -0,0 +1,32 @@
+/*
+ * AquaPi - sterownik akwariowy oparty o Raspberry Pi
+ *
+ * Copyright (C) 2012 Marcin Król (lexx@polarnet.pl)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License Version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ * USA.
+ *
+ * $Id:$
+ */
+
+#define APPNAME "AquaPi"
+
+const int E_DEV = -1;
+const int E_INFO = 0;
+const int E_WARN = 1;
+const int E_CRIT = 2;
+
+void Log(char *msg, int lev);
+
+void termination_handler(int signum);
