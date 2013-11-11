55da1010bd3e78dfd9a5370e5be00c09f846eb43
SirYaro <jaroslaw@czarniak.org>
2013-11-11 18:48:35 +0100

    rozbudowa klasy bazodanowej

--- Software/WebUi/lib/database.class.php
+++ Software/WebUi/lib/database.class.php
@@ -2,7 +2,9 @@
 /*
  * AquaPi - sterownik akwariowy oparty o Raspberry Pi
  *
- * Copyright (C) 2012 Marcin Kr�l (lexx@polarnet.pl)
+ * Copyright (C) 2012 Marcin Król (lexx@polarnet.pl)
+ * Copyright (C) 2013 Jarosław Czarniak (jaroslaw@czarniak.org)
+ * Copyright (C) 2001-2011 LMS Developers
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License Version 2 as
@@ -21,14 +23,20 @@
  * $Id$
  */
  
-// inicjalizacja bazy danych
 
 class Database {
 
-	var $_dblink;
-	var $_result;
-	function Database($dbhost, $dbuser, $dbpasswd, $dbname) {
+	var $_dblink	= NULL;
+	var $_result	= NULL;
+	var $_query	= NULL;
+	var $errors	= array();
+	var $debug	= FALSE;
 
+//
+//	FUNKCJE
+//
+
+	function Database($dbhost, $dbuser, $dbpasswd, $dbname) {
 		$this->_dblink = mysql_connect($dbhost, $dbuser, $dbpasswd);
 		if (!$this->_dblink) {
 			die('Brak polaczenia z baza: ' . mysql_error());
@@ -40,35 +48,159 @@ class Database {
 		$this->Execute("SET NAMES utf8;");
 	}
 
-	function Execute($query) {
-		$this->_result = @mysql_query($query, $this->_dblink);
-	}
-
-	function GetAll($query) {
-		//echo $query."<BR>";
-		$this->Execute($query);
-		$result = NULL;
-		while($row = @mysql_fetch_array($this->_result, MYSQL_ASSOC))
-		$result[] = $row;
-		return $result;
-	}
-
-	function GetRow($query) 
+	function Execute($query, $inputarray = NULL)
 	{
-		//echo $query."<BR>";
-		$this->Execute($query);
+
+		if(! $this->_driver_execute($this->_query_parser($query, $inputarray)))
+			$this->errors[] = array(
+					'query' => $this->_query,
+					'error' => $this->_driver_geterror()
+					);
+		elseif($this->debug)
+			$this->errors[] = array(
+					'query' => $this->_query,
+					'error' => 'DEBUG: NOERROR'
+					);
+		return $this->_driver_affected_rows();
+	}
+
+	function GetAll($query = NULL, $inputarray = NULL)
+	{
+		if($query)
+			$this->Execute($query, $inputarray);
+
 		$result = NULL;
-		$row = mysql_fetch_array($this->_result, MYSQL_ASSOC);
-		$result = $row;
+
+		while($row = $this->_driver_fetchrow_assoc())
+			$result[] = $row;
+		
 		return $result;
 	}
 
-	function GetOne($query) {
-		$this->Execute($query);
+
+	function GetRow($query = NULL, $inputarray = NULL)
+	{
+		if($query)
+			$this->Execute($query, $inputarray);
+
+		return $this->_driver_fetchrow_assoc();
+	}
+
+
+	function GetOne($query = NULL, $inputarray = NULL)
+	{
+		if($query)
+			$this->Execute($query, $inputarray);
+
 		$result = NULL;
-		list($result) = @mysql_fetch_array($this->_result, MYSQL_NUM);
+
+		list($result) = $this->_driver_fetchrow_num();
+
 		return $result;
 	}
+
+
+//
+//	DRIVER
+//
+	function _driver_execute($query)
+	{
+		$this->_query = $query;
+
+		if($this->_result = @mysql_query($query, $this->_dblink))
+			$this->_error = FALSE;
+		else
+			$this->_error = TRUE;
+		return $this->_result;
+	}
+
+	function _query_parser($query, $inputarray = NULL)
+	{
+
+		// najpierw sparsujmy wszystkie specjalne meta śmieci.
+		$query = preg_replace('/\?NOW\?/i',$this->_driver_now(),$query);
+		$query = preg_replace('/\?LIKE\?/i',$this->_driver_like(),$query);
+		if($inputarray)
+		{
+			$queryelements = explode("\0",str_replace('?',"?\0",$query));
+			$query = '';
+			foreach($queryelements as $queryelement)
+			{
+				if(strpos($queryelement,'?') !== FALSE)
+				{
+					list($key,$value) = each($inputarray);
+					$queryelement = str_replace('?',$this->_quote_value($value),$queryelement);
+				}
+				$query .= $queryelement;
+			}
+		}
+		//echo $query."<BR>";
+		return $query;
+	}
+
+	function _quote_value($input)
+	{
+		// jeżeli baza danych wymaga innego eskejpowania niż to, driver
+		// powinien nadpisać tą funkcję
+
+		if($input === NULL)
+			return 'NULL';
+		elseif(gettype($input) == 'string')
+			return '\''.addcslashes($input,"'\\\0").'\'';
+		else
+			return $input;
+	}
+
+	function _driver_now()
+	{
+		return time();
+	}
+
+	function _driver_like()
+	{
+		return 'LIKE';
+	}
+
+	function _driver_setencoding($name)
+	{
+		$this->Execute('SET NAMES ?', array($name));
+	}
+
+	function _driver_fetchrow_num()
+	{
+		if(! $this->_error)
+			return mysql_fetch_array($this->_result, MYSQL_NUM);
+		else
+			return FALSE;
+	}
+
+	function _driver_affected_rows()
+	{
+		if(! $this->_error)
+			return mysql_affected_rows();
+		else
+			return FALSE;
+	}
+
+	function _driver_geterror()
+	{
+		if($this->_dblink)
+			return mysql_error($this->_dblink);
+		elseif($this->_query)
+			return 'We\'re not connected!';
+		else
+			return mysql_error();
+	}
+
+	function _driver_fetchrow_assoc($result = NULL)
+	{
+		if(! $this->_error)
+			return mysql_fetch_array($result ? $result : $this->_result, MYSQL_ASSOC);
+		else
+			return FALSE;
+	}
+
+
 }
 
 ?>
