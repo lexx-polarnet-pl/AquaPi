b66c1a2444766629faf8b36adcfe1183c5b69de8
SirYaro <jaroslaw@czarniak.org>
2014-03-15 18:05:02 +0100

    uruchomienie wykresów. brak wsparcia dla brakujacych danych. (trzeba by generowac null przy wyciaganiu danych)

--- Software/WebUi/jsonp.php
+++ Software/WebUi/jsonp.php
@@ -3,11 +3,16 @@
 include("init.php");
 
 $dane   = '';
-$stats	= $db->GetAll('SELECT stat_date as E, stat_value as N FROM stats WHERE stat_date >= ? AND stat_interfaceid=? ORDER BY stat_date ', array($_GET['limit'], $_GET['interfaceid']));
+$stats	= $db->GetAll('SELECT stat_date as E, stat_value as N
+                      FROM stats
+                      WHERE stat_date >= ? AND stat_interfaceid=? ORDER BY stat_date ',
+                      array($_GET['limit'], $_GET['interfaceid']));
+
 if($_GET['simplify_graphs']==1)
     $stats  = RamerDouglasPeucker($stats, 0.1);
+
 foreach ($stats as $stat)
-    $dane .= '[' . $stat['E'] . ',' . $stat['N'] . '],';
+    $dane .= '[' . $stat['E'] . '000,' . $stat['N'] . '],';
 
 $callback = (string)$_GET['callback'];
 
