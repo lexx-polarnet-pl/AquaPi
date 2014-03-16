ec9bcc3ff2b42eea5a420d48411179830d9f368f
Jarosław Czarniak <jaroslaw@czarniak.org>
2014-03-16 15:35:06 +0100

    Update inifile.c
    _

--- Software/daemon/inifile.c
+++ Software/daemon/inifile.c
@@ -48,10 +48,10 @@ static int handler(void* user, const char* section, const char* name, const char
         pconfig->reload_freq = atoi(value);		
     } else if (MATCH("daemon", "dummy_temp_sensor")) {
         pconfig->dummy_temp_sensor_val = atof(value);
-    } else if (MATCH("daemon", "interface")) {
-        pconfig->interface = strdup(value);		
-    } else if (MATCH("daemon", "port")) {
-        pconfig->port = atoi(value);		
+    } else if (MATCH("daemon", "bind_address")) {
+        pconfig->bind_address = strdup(value);		
+    } else if (MATCH("daemon", "bind_port")) {
+        pconfig->bind_port = atoi(value);		
     } else {
         return 0;  /* unknown section/name, error */
     }
