00596e38d9d3cb3dc0c0ccac3eb1ae926277496e
SirYaro <jaroslaw@czarniak.org>
2014-03-16 14:36:11 +0100

    dopasowanie do nowego konfiga

--- Software/WebUi/lib/ipc.php
+++ Software/WebUi/lib/ipc.php
@@ -22,97 +22,9 @@
  */
 
 
-$host = "localhost";
-$port = 6580;
+$host	= $CONFIG['daemon']['host'];
+$port	= $CONFIG['daemon']['port'];
 
-/*
-function IPC_Command($command) {
-	global $myfifo;
-	$fp=fopen($myfifo, "r+"); // ensures at least one writer (us) so will be non-blocking
-	stream_set_blocking($fp, false);
-	fwrite($fp, "aquapi:".$command);
-	fclose($fp); 
-}
-
-function IPC_GetPid() {
-	global $myfifo,$myfifo2;
-	IPC_Command("about");
-
-	$fp2=fopen($myfifo2, "r+"); // ensures at least one writer (us) so will be non-blocking
-	stream_set_blocking($fp2, false);
-	$failcount =0;
-	do {
-	    if ($failcount > 0) usleep(100000);
-	    $failcount++;
-	    $data = fread($fp2,1024);
-	    if (strrpos($data,"aquapi>about>end of reply") > 0) break;
-	} while ($failcount < 3);
-	fclose($fp2); 
-
-	if ($failcount == 3) {
-	    $ret = -1;
-	} else {
-	    foreach (explode("\n",$data) as $line) {
-		if (strstr($line, "aquapi>about>PID:")!==false) {
-			$ret = intval(substr($line,17));
-		}
-	    }
-	}
-	return $ret;
-}
-
-function IPC_GetDaemonData() {
-	global $myfifo,$myfifo2;
-	IPC_Command("about");
-
-	$fp2=fopen($myfifo2, "r+"); // ensures at least one writer (us) so will be non-blocking
-	stream_set_blocking($fp2, false);
-	$failcount =0;
-	do {
-	    if ($failcount > 0) usleep(100000);
-	    $failcount++;
-	    $data = fread($fp2,1024);
-	    if (strrpos($data,"aquapi>about>end of reply") > 0) break;
-	} while ($failcount < 3);
-	fclose($fp2); 
-
-	if ($failcount == 3) {
-	    $pid = -1;
-	} else {
-	    foreach (explode("\n",$data) as $line) {
-		if (strstr($line, "aquapi>about>PID:")!==false) {
-			$pid = intval(substr($line,17));
-		}
-		if (strstr($line, "aquapi>about>compilation:")!==false) {
-			$compilation = substr($line,25);
-		}		
-	    }
-	}
-	return array("pid" => $pid, "compilation" => $compilation);
-}
-
-function IPC_CommandWithReply($command) {
-	global $myfifo,$myfifo2;
-	IPC_Command($command);
-
-	$fp2=fopen($myfifo2, "r+"); // ensures at least one writer (us) so will be non-blocking
-	stream_set_blocking($fp2, false);
-	$failcount =0;
-	do {
-	    if ($failcount > 0) usleep(100000);
-	    $failcount++;
-	    $data = fread($fp2,10000);
-	    if (strrpos($data,"<reply type=\"".$command."\"/>") > 0) break;
-	} while ($failcount < 3);
-	fclose($fp2); 
-
-	if ($failcount == 10) {
-	    $ret = -1;
-	} else {
-	    $ret = $data;
-	}
-	return $ret;
-}*/
 
 function IPC_Command($command) {
 	global $host,$port;
