91cc70dca33acbdceb8e3859555976a448c7c092
Lexx <lexx@polarnet.pl>
2014-03-17 20:37:32 +0100

    Usunięcie zbędnych $Id$

--- Software/WebUi/init.php
+++ Software/WebUi/init.php
@@ -18,7 +18,6 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
  * USA.
  *
- * $Id: 1202746a7d2d546d168e6b25a9e32c8bdbc83b8b $
  */
 
 // wersja AquaPi
--- Software/WebUi/lib/database.class.php
+++ Software/WebUi/lib/database.class.php
@@ -19,7 +19,6 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
  * USA.
  *
- * $Id: c160e6a977321acd19442bb62596b568c8cfc816 $
  */
 
 
