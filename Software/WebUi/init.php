9ab944aa9d6048b5715962e261a8359e33287123
lexx <lexx@polarnet.pl>
2018-12-09 21:45:54 +0100

    nie pokazuj bledow

--- Software/WebUi/init.php
+++ Software/WebUi/init.php
@@ -21,9 +21,9 @@
  */
  
 // robimy dobie debug
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
 
 // wersja AquaPi
 $aquapi_ver = "1.9-devel";
